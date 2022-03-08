58e0cf4101fcfd0dd3ee582711b409211edb899f
Benjamin Bala <benjamin.bala.91@gmail.com>
2022-03-08 01:47:19 -0700

    Added more projects to portfolio

--- src/projects.js
+++ src/projects.js
@@ -7,8 +7,24 @@ export const projects = [
         project_thumbnail:  'https://res.cloudinary.com/benjee/image/upload/v1623680219/project-1_ttg9dd.png',
         project_repo: 'https://github.com/benjamin-bala/watch-list'
     },
-    {
+     {
         project_id: '02',
+        project_name: 'Hivestore',
+        project_description: 'A book store built with react \n which allows users to buy course material',
+        project_link: 'https://hivestore.netlify.app/',
+        project_thumbnail:  'https://res.cloudinary.com/benjee/image/upload/v1646728881/hivestore_tmxe5l.png',
+        project_repo: 'https://github.com/benjamin-bala/bookstore'
+    },
+    {
+        project_id: '03',
+        project_name: 'Cat Directory',
+        project_description: 'A cat directory built with react native \n that lists out various cat breeds',
+        project_link: 'https://drive.google.com/file/d/1j90j5DJxU-yfh5oi8x-m8vfzPmqVW8At/view',
+        project_thumbnail:  'https://res.cloudinary.com/benjee/image/upload/v1646729159/trap-the-cat-18-apk_moevxk.jpg',
+        project_repo: 'https://github.com/benjamin-bala/backdrop-coding-challenge'
+    },
+    {
+        project_id: '04',
         project_name: 'IP Address Tracker',
         project_description: 'A Frontend Mentor Challenege. \n App was built using Html,Css,Javascript',
         project_link: 'https://practical-chandrasekhar-4515fd.netlify.app/',
@@ -16,7 +32,7 @@ export const projects = [
         project_repo: 'https://github.com/benjamin-bala/ip-address-tracker'
     },
     {
-        project_id: '03',
+        project_id: '05',
         project_name: 'Signup Form',
         project_description: 'A Frontend Mentor Challenege. \n Page was built using Html & Css',
         project_link: 'https://kind-snyder-b721e3.netlify.app/',
@@ -24,7 +40,7 @@ export const projects = [
         project_repo: 'https://github.com/benjamin-bala/signup-form'
     },
     {
-        project_id: '04',
+        project_id: '06',
         project_name: 'Country Directory',
         project_description: 'A Frontend Mentor Challenege. \n App was built using react.js',
         project_link: 'https://nervous-panini-1916d4.netlify.app/',
@@ -32,7 +48,7 @@ export const projects = [
         project_repo: 'https://github.com/benjamin-bala/countries/tree/master'
     },
     {
-        project_id: '05',
+        project_id: '07',
         project_name: 'Buy Food Onboarding',
         project_description: 'Onboarding page for Food tech',
         project_link: 'https://buyfood-onboarding.netlify.app/',
@@ -40,7 +56,7 @@ export const projects = [
         project_repo: 'https://github.com/benjamin-bala/buyfood-onboarding'
     },
     {
-        project_id: '06',
+        project_id: '08',
         project_name: 'Send Freight',
         project_description: 'This is a demo app for courier service booking',
         project_link: 'https://freight-demo.netlify.app/',
